725d0839c10310c2761780a4de9846070cb3253e
Joy Okwudire <okwudirejoy@gmail.com>
2022-02-01 03:33:14 +0100

    desktop navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
 
 const Toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <div>Menu</div>
       <Logo />
-      <nav>...</nav>
+      <nav>
+        <NavigationItems />
+      </nav>
     </header>
   );
 };
